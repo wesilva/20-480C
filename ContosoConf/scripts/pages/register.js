69a436020df8a0128ffe45eed08eb72f01474bdf
Wellington <wellingtonti23@gmail.com>
2019-12-19 06:39:09 -0400

    Module 4 - Exercise 1 - Task 2 - Add form inputs to the Register page

---
+++ ContosoConf/scripts/pages/register.js
@@ -0,0 +1,22 @@
+﻿const form = document.getElementById('registration-form');
+const submitButton = form.querySelector('button');
+
+const checkPasswords = function () {
+
+};
+
+
+const addPasswordInputEventListeners = function () {
+
+};
+
+const formSubmissionAttempted = function () {
+    form.classList.add('submission-attempted');
+}
+
+const addSubmitClickEventListener = function () {
+    submitButton.addEventListener('click', formSubmissionAttempted, false);
+};
+
+addPasswordInputEventListeners();
+addSubmitClickEventListener();
