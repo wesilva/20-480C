12ffe65f83915351abc642539596d6333590be4e
Wellington <wellingtonti23@gmail.com>
2019-12-28 17:53:00 -0400

    Module 4 Exercise 1 Task 2 - Add backend

---
+++ ContosoConf/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using ContosoConf.App_Start;
+using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ContosoConf
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
