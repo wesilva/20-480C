43f19dc479d28d8bb38693d12876d632f6394f77
Wellington <wellingtonti23@gmail.com>
2020-01-02 13:27:51 -0400

    Module 4 - Exercise 2 - Validating User Input By usiang JavaScript

--- ContosoConf/scripts/pages/register.js
+++ ContosoConf/scripts/pages/register.js
@@ -1,13 +1,22 @@
 ﻿const form = document.getElementById('registration-form');
 const submitButton = form.querySelector('button');
 
-const checkPasswords = function () {
+const passwordInput = document.getElementById('password');
+const confirmPasswordInput = document.getElementById('confirm-password');
 
+
+const checkPasswords = function () {
+    if (passwordInput.value === confirmPasswordInput.value) {
+        confirmPasswordInput.setCustomValidity('');
+    } else {
+        confirmPasswordInput.setCustomValidity(`Your passwords don't match. Please type the same password again.`);
+    }
 };
 
 
 const addPasswordInputEventListeners = function () {
-
+    passwordInput.addEventListener('input', checkPasswords, false);
+    confirmPasswordInput.addEventListener('input', checkPasswords, false);
 };
 
 const formSubmissionAttempted = function () {
